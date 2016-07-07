89ba2be663c9936a0b94d4d7f4e7815255fd8c74
victsomie <victsomie@yahoo.com>
2016-07-07 13:57:49 +0300

    added reference to elements in the cards inside the Card rcycler adpater

--- app/src/main/java/com/example/victor/recyclerandcards/CardRecyclerAdapter.java
+++ app/src/main/java/com/example/victor/recyclerandcards/CardRecyclerAdapter.java
@@ -3,6 +3,9 @@ package com.example.victor.recyclerandcards;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by victor on 7/7/16.
@@ -14,11 +17,27 @@ public class CardRecyclerAdapter extends RecyclerView.Adapter<CardRecyclerAdapte
     //This represents how the text will be displayed
     public class DataHolder extends RecyclerView.ViewHolder{
 
+        //Reference the items in your card by ID and initialize them inside the Dataholder constructor below
+        TextView eventTitle;
+        TextView eventPoster;
+        TextView eventDetails;
+        ImageView eventImage;
+        Button eventJoin;
+        Button eventLike;
+        Button eventShare;
+
 
         public DataHolder(View itemView) {
             super(itemView);
 
-
+            //references below
+            eventImage = (ImageView) itemView.findViewById(R.id.imgUser);
+            eventTitle = (TextView) itemView.findViewById(R.id.textEventTitle);
+            eventPoster = (TextView) itemView.findViewById(R.id.textPostedby);
+            eventDetails = (TextView) itemView.findViewById(R.id.textEventDetails);
+            eventJoin= (Button) itemView.findViewById(R.id.buttonJoin);
+            eventLike= (Button) itemView.findViewById(R.id.buttonLike);
+            eventShare= (Button) itemView.findViewById(R.id.buttonShare);
 
         }
     }
