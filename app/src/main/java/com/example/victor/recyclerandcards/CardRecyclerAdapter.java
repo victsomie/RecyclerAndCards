4f88b39c31d8f6970a716646f3050392306b43c8
victsomie <victsomie@yahoo.com>
2016-07-07 14:38:07 +0300

    added main class constructor,  DataHolder(to inflate the card view) and added binding to items in the ArrayList(mDataSet) with the ones inside the DataHolder (which is the cardview)

--- app/src/main/java/com/example/victor/recyclerandcards/CardRecyclerAdapter.java
+++ app/src/main/java/com/example/victor/recyclerandcards/CardRecyclerAdapter.java
@@ -1,16 +1,21 @@
 package com.example.victor.recyclerandcards;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by victor on 7/7/16.
  */
 public class CardRecyclerAdapter extends RecyclerView.Adapter<CardRecyclerAdapter.DataHolder> {
+
+    private ArrayList<DataObject> mDataset;//Arraylist that will hold the data
 
 
     //Create the data holder and #Create its constructor (That matches )
@@ -44,14 +49,35 @@ public class CardRecyclerAdapter extends RecyclerView.Adapter<CardRecyclerAdapte
 
 
 
+    //This is your class constructor that will take Arraylist containing DataObject(you definedd this in a different class) you created
+    public CardRecyclerAdapter(ArrayList<DataObject> mDataset) {
+        mDataset = mDataset;
+    }
+
+
+
+    //Define which view t recycle
     @Override
     public DataHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        //Inflate you card view
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.my_card, parent, false);
+
+        DataHolder dataHolder = new DataHolder(view); //This say, our data holder viewis the view we have defined above(Which is our card)
+
+        return dataHolder; //Returns that dataHolder, which now is the card
     }
 
     @Override
     public void onBindViewHolder(DataHolder holder, int position) {
 
+
+        //Inside the onBindViewHolder, bind the items in the arralist(mDataSet) to the ones in your card view (in the DataHolder)
+        holder.eventTitle.setText(mDataset.get(position).getmText1());
+        holder.eventPoster.setText(mDataset.get(position).getmText2());
+        holder.eventDetails.setText(mDataset.get(position).getmText3());
     }
 
     @Override
